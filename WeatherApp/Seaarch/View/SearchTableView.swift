f0b3fd65a227027685d081c7795d96d62844f22d
Mateo Došlić <mateo55@live.com>
2018-07-27 12:21:57 +0200

    searchView Implementation

---
+++ WeatherApp/Seaarch/View/SearchTableView.swift
@@ -0,0 +1,15 @@
+//
+//  SearchTableView.swift
+//  WeatherApp
+//
+//  Created by Mateo Došlić on 27/07/2018.
+//  Copyright © 2018 Mateo Došlić. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableView: UIView {
+    
+
+
+}
---
+++ WeatherApp/WeatherCell/WeatherViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherViewCell.swift
+//  WeatherApp
+//
+//  Created by Mateo Došlić on 27/07/2018.
+//  Copyright © 2018 Mateo Došlić. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewCell: UITableViewCell {
+
+
+
+}
